87947137d0e791344e1072ea7f768b30f0966091
Dumaru <robinsonmu@unisabana.edu.co>
2020-06-15 10:10:35 -0500

    Cambios db sql

--- database.sql
+++ database.sql
@@ -4,8 +4,8 @@ CREATE DATABASE IF NOT EXISTS ladrillera;
 
 USE ladrillera;
 
--- Creacion tabla empleados
-CREATE TABLE empleados (
+-- Creacion tabla EMPLEADOS
+CREATE TABLE EMPLEADOS (
     id_empleado INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(250),
     apellido VARCHAR(250),
@@ -17,54 +17,53 @@ CREATE TABLE empleados (
 );
 
 INSERT INTO
-    empleados (id_empleado, nombre)
+    EMPLEADOS (id_empleado, nombre)
 values
     (NULL, "Robinson"),
-    (NULL, "Ana"),
-;
+    (NULL, "Ana");
 
--- Creacion tabla usuarios
-CREATE TABLE usuarios (
+-- Creacion tabla USUARIOS
+CREATE TABLE USUARIOS (
     id_usuario INT(11) NOT NULL AUTO_INCREMENT,
     id_empleado INT(11) NOT NULL,
-    usuarios VARCHAR(250),
+    usuario VARCHAR(250),
     contraseña VARCHAR(250),
     PRIMARY KEY (id_usuario),
-    FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado) ON DELETE CASCADE
+    FOREIGN KEY (id_empleado) REFERENCES EMPLEADOS(id_empleado) ON DELETE CASCADE
 );
 
--- Creacion tabla pedidos
-CREATE TABLE pedidos (
+-- Creacion tabla PEDIDOS
+CREATE TABLE PEDIDOS (
     id_pedido INT(11) NOT NULL AUTO_INCREMENT,
-    pedidos VARCHAR(500),
+    PEDIDO VARCHAR(500),
     cantidad INT(15),
     descripcion TEXT,
     PRIMARY KEY (id_pedido)
 );
 
 -- Creacion tabla pedido_empleado
-CREATE TABLE pedido_empleado (
+CREATE TABLE PEDIDO_EMPLEADO (
     id_pedido INT(11) NOT NULL,
     id_empleado INT(11) NOT NULL,
     PRIMARY KEY (id_empleado),
-    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido) ON DELETE CASCADE,
-    FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado) ON DELETE CASCADE
+    FOREIGN KEY (id_pedido) REFERENCES PEDIDOS(id_pedido) ON DELETE CASCADE,
+    FOREIGN KEY (id_empleado) REFERENCES EMPLEADOS(id_empleado) ON DELETE CASCADE
 );
 
--- Creacion tabla validacion
-CREATE TABLE validacion (
+-- Creacion tabla VALIDACIONES
+CREATE TABLE VALIDACIONES (
     id_validacion INT(11) NOT NULL AUTO_INCREMENT,
     id_empleado INT(11) NOT NULL,
     id_pedido INT(11) NOT NULL,
     anexo VARCHAR(400),
     descripcion TEXT,
     PRIMARY KEY (id_validacion),
-    FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado) ON DELETE CASCADE,
-    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido) ON DELETE CASCADE
+    FOREIGN KEY (id_empleado) REFERENCES EMPLEADOS(id_empleado) ON DELETE CASCADE,
+    FOREIGN KEY (id_pedido) REFERENCES PEDIDOS(id_pedido) ON DELETE CASCADE
 );
 
--- Creacion tabla impresiones
-CREATE TABLE impresiones (
+-- Creacion tabla IMPRESIONES
+CREATE TABLE IMPRESIONES (
     id_impresion INT(11) NOT NULL AUTO_INCREMENT,
     id_pedido INT(11) NOT NULL,
     impreso VARCHAR(300),
@@ -72,8 +71,8 @@ CREATE TABLE impresiones (
     FOREIGN KEY (id_pedido) REFERENCES pedido_empleado(id_pedido) ON DELETE CASCADE
 );
 
--- Creacion tabla clientes
-CREATE TABLE clientes (
+-- Creacion tabla CLIENTES
+CREATE TABLE CLIENTES (
     id_cliente INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(250),
     apellido VARCHAR(250),
@@ -81,8 +80,8 @@ CREATE TABLE clientes (
     PRIMARY KEY (id_cliente)
 );
 
--- Creacion tabla documentos
-CREATE TABLE documentos(
+-- Creacion tabla DOCUMENTOS
+CREATE TABLE DOCUMENTOS(
     id_documento INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(250),
     descripcion TEXT,
@@ -91,16 +90,16 @@ CREATE TABLE documentos(
 );
 
 -- Creacion tabla cliente_documento
-CREATE TABLE cliente_documento(
+CREATE TABLE CLIENTE_DOCUMENTO(
     id_documento INT(11) NOT NULL,
     id_cliente INT(11) NOT NULL,
     PRIMARY key(id_documento),
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE,
-    FOREIGN KEY (id_documento) REFERENCES documentos(id_documento) ON DELETE CASCADE
+    FOREIGN KEY (id_cliente) REFERENCES CLIENTES(id_cliente) ON DELETE CASCADE,
+    FOREIGN KEY (id_documento) REFERENCES DOCUMENTOS(id_documento) ON DELETE CASCADE
 );
 
--- Creacion tabla reclamos
-CREATE TABLE reclamos(
+-- Creacion tabla RECLAMOS
+CREATE TABLE RECLAMOS(
     id_reclamo INT(11) NOT NULL AUTO_INCREMENT,
     tipo VARCHAR(200),
     descripcion TEXT,
@@ -108,20 +107,20 @@ CREATE TABLE reclamos(
 );
 
 -- Creacion tabla cliente_pedido
-CREATE TABLE cliente_pedido(
+CREATE TABLE CLIENTE_PEDIDO(
     id_pedido INT(11) NOT NULL,
     id_cliente INT(11) NOT NULL,
     estacion VARCHAR(300),
     PRIMARY KEY (id_pedido),
-    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido) ON DELETE CASCADE,
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE
+    FOREIGN KEY (id_pedido) REFERENCES PEDIDOS(id_pedido) ON DELETE CASCADE,
+    FOREIGN KEY (id_cliente) REFERENCES CLIENTES(id_cliente) ON DELETE CASCADE
 );
 
 -- Creacion tabla cliente_pedido
-CREATE TABLE pedido_reclamo(
+CREATE TABLE PEDIDO_RECLAMO(
     id_pedido INT(11) NOT NULL,
     id_reclamo INT(11) NOT NULL,
     PRIMARY KEY (id_pedido),
-    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido) ON DELETE CASCADE,
-    FOREIGN KEY (id_reclamo) REFERENCES reclamos(id_reclamo) ON DELETE CASCADE
+    FOREIGN KEY (id_pedido) REFERENCES PEDIDOS(id_pedido) ON DELETE CASCADE,
+    FOREIGN KEY (id_reclamo) REFERENCES RECLAMOS(id_reclamo) ON DELETE CASCADE
 );
