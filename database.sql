aa8e821a6b1f9b489cd261c24f011b612925fd64
sebasgaspar <sebasgaspar2009@hotmail.com>
2020-06-22 23:08:37 -0500

    Sql

--- database.sql
+++ database.sql
@@ -6,14 +6,14 @@ USE ladrillera;
 
 -- Creacion tabla EMPLEADOS
 CREATE TABLE EMPLEADOS (
-    id_empleado INT(11) NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(250),
     apellido VARCHAR(250),
     cedula_ciudadania INT(11),
     genero VARCHAR(20),
     fecha_nacimiento DATE,
     rol VARCHAR(250),
-    PRIMARY KEY (id_empleado)
+    PRIMARY KEY (id)
 );
 
 INSERT INTO
@@ -24,65 +24,65 @@ values
 
 -- Creacion tabla USUARIOS
 CREATE TABLE USUARIOS (
-    id_usuario INT(11) NOT NULL AUTO_INCREMENT,
-    id_empleado INT(11) NOT NULL,
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    id_E INT(11) NOT NULL,
     usuario VARCHAR(250),
     contraseña VARCHAR(250),
-    PRIMARY KEY (id_usuario),
-    FOREIGN KEY (id_empleado) REFERENCES EMPLEADOS(id_empleado) ON DELETE CASCADE
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) ON DELETE CASCADE
 );
 
 -- Creacion tabla PEDIDOS
 CREATE TABLE PEDIDOS (
-    id_pedido INT(11) NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     PEDIDO VARCHAR(500),
     cantidad INT(15),
     descripcion TEXT,
-    PRIMARY KEY (id_pedido)
+    PRIMARY KEY (id)
 );
 --Creacion tabla MODULO
 CREATE TABLE MODULO (
-    id_modulo INT(11) NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(200),
     
-    PRIMARY KEY (id_modulo)
+    PRIMARY KEY (id)
 );
 -- Creacion tabla EMPLEADO_MODULO
 CREATE TABLE EMPLEADO_MODULO (
-    id_modulo INT(11) NOT NULL,
-    id_empleado INT(11) NOT NULL,
+    id INT(11) NOT NULL,
+    id_E INT(11) NOT NULL,
 
-    FOREIGN KEY (id_modulo) REFERENCES MODULO(id_modulo) ON DELETE CASCADE,
-    FOREIGN KEY (id_empleado) REFERENCES EMPLEADOS(id_empleado) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES MODULO(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) ON DELETE CASCADE
 );
 
 -- Creacion tabla VALIDACIONES
 CREATE TABLE VALIDACIONES (
-    id_validacion INT(11) NOT NULL AUTO_INCREMENT,
-    id_empleado INT(11) NOT NULL,
-    id_pedido INT(11) NOT NULL,
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    id_E INT(11) NOT NULL,
+    id_P INT(11) NOT NULL,
     anexo VARCHAR(400),
     descripcion TEXT,
-    PRIMARY KEY (id_validacion),
-    FOREIGN KEY (id_empleado) REFERENCES EMPLEADOS(id_empleado) ON DELETE CASCADE,
-    FOREIGN KEY (id_pedido) REFERENCES PEDIDOS(id_pedido) ON DELETE CASCADE
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_P) REFERENCES PEDIDOS(id) ON DELETE CASCADE
 );
 
 -- Creacion tabla CLIENTES
 CREATE TABLE CLIENTES (
-    id_cliente INT(11) NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(250),
     apellido VARCHAR(250),
     cedula_ciudadania INT(15),
-    PRIMARY KEY (id_cliente)
+    PRIMARY KEY (id)
 );
 --Creacion tabla EMPLEADOS_CLIENTES
 CREATE TABLE EMPLEADOS_CLIENTES (
-    id_cliente INT(11) NOT NULL AUTO_INCREMENT,
-    id_empleado INT(11) NOT NULL,
+    id_C INT(11) NOT NULL,
+    id_E INT(11) NOT NULL,
 
-    FOREIGN KEY (id_cliente) REFERENCES CLIENTES(id_cliente) ON DELETE CASCADE,
-    FOREIGN KEY (id_empleado) REFERENCES EMPLEADOS(id_empleado) ON DELETE CASCADE
+    FOREIGN KEY (id_C) REFERENCES CLIENTES(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) ON DELETE CASCADE
 );
 
 -- Creacion tabla DOCUMENTOS
