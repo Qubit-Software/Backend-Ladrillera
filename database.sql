a7e1c851361bf0d2a600159ce003eeb64041f6ce
sebasgaspar <sebasgaspar2009@hotmail.com>
2020-07-08 00:12:19 -0500

    SQL UPDATE

--- database.sql
+++ database.sql
@@ -10,9 +10,12 @@ CREATE TABLE EMPLEADOS (
     nombre VARCHAR(250),
     apellido VARCHAR(250),
     cedula_ciudadania INT(11),
+    correo VARCHAR(200),
     genero VARCHAR(20),
     fecha_nacimiento DATE,
     rol VARCHAR(250),
+    foto VARCHAR(200),
+
     PRIMARY KEY (id)
 );
 
@@ -28,35 +31,43 @@ CREATE TABLE USUARIOS (
     id_E INT(11) NOT NULL,
     usuario VARCHAR(250),
     contraseña VARCHAR(250),
+    activo boolean,
+
     PRIMARY KEY (id),
     FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) ON DELETE CASCADE
 );
 -- Creacion tabla CLIENTES
 CREATE TABLE CLIENTES (
     id INT(11) NOT NULL AUTO_INCREMENT,
+    id_E INT(11) NOT NULL,
     nombre VARCHAR(250),
     apellido VARCHAR(250),
-    cedula_ciudadania INT(15),
+    cc_nit INT(15),
     tipo_cliente VARCHAR(100),
-    PRIMARY KEY (id)
+    ciudad VARCHAR(150),
+    correo VARCHAR(150),
+    telefono VARCHAR(100),
+
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id)
 );
 -- Creacion tabla PEDIDOS
 CREATE TABLE PEDIDOS (
     id INT(11) NOT NULL AUTO_INCREMENT,
     id_C INT(11) NOT NULL,
-    PEDIDO VARCHAR(500),
-    cantidad INT(15),
-    descripcion TEXT,
+    id_E INT(11) NOT NULL,
+    fecha_cargue date,
+    comentario TEXT,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_C) REFERENCES CLIENTES(id) ON DELETE CASCADE
-
+    FOREIGN KEY (id_C) REFERENCES CLIENTES(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) 
 );
 --Creacion Productos
 CREATE TABLE PRODUCTOS (
     id INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(200),
     descripcion TEXT,
-    imagen VARCHAR(200),
 
     PRIMARY KEY (id)
 );
@@ -64,6 +75,8 @@ CREATE TABLE PRODUCTOS (
 CREATE TABLE PRODUCTOS_PEDIDOS (
     id_P INT(11) NOT NULL,
     id_Pr INT(11) NOT NULL,
+    cantidad INT(15),
+    unid_medicion INT(15),
 
     FOREIGN KEY (id_P) REFERENCES PEDIDOS(id) ON DELETE CASCADE,
     FOREIGN KEY (id_Pr) REFERENCES PRODUCTOS(id) ON DELETE CASCADE
@@ -92,28 +105,19 @@ CREATE TABLE VALIDACIONES (
     id_E INT(11) NOT NULL,
     id_P INT(11) NOT NULL,
     fase INT(3) NOT NULL,
-    anexo VARCHAR(400),
-    descripcion TEXT,
+    registro_fotografico VARCHAR(200),
+    comentarios TEXT,
     PRIMARY KEY (id),
     FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) ON DELETE CASCADE,
     FOREIGN KEY (id_P) REFERENCES PEDIDOS(id) ON DELETE CASCADE
 );
---Creacion tabla EMPLEADOS_CLIENTES
-CREATE TABLE EMPLEADOS_CLIENTES (
-    id_C INT(11) NOT NULL,
-    id_E INT(11) NOT NULL,
-
-    FOREIGN KEY (id_C) REFERENCES CLIENTES(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_E) REFERENCES EMPLEADOS(id) ON DELETE CASCADE
-);
-
 -- Creacion tabla DOCUMENTOS
 CREATE TABLE DOCUMENTOS(
     id INT(11) NOT NULL AUTO_INCREMENT,
     id_C INT(11) NOT NULL,
     nombre VARCHAR(250),
-    descripcion TEXT,
     tipoArchivo VARCHAR(250),
+    
     PRIMARY key (id),
     FOREIGN KEY (id_C) REFERENCES CLIENTES(id) ON DELETE CASCADE
 );
